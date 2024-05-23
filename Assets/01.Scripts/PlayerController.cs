826f7c63e0769a8b6c35b4dc6df28cc6bdd6f11d
pjhee07 <pjhee07@naver.com>
2024-05-23 23:40:46 +0900

    폴더들, 플레이어스크립트

---
+++ Assets/01.Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigid;
+    [SerializeField] private int jumpPower;
+    [SerializeField] private int moveSpeed;
+    private bool isJumping;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        isJumping = false;
+    }
+    private void Update()
+    {
+        PlayerMove();
+        PlayerJump();
+    }
+    private void PlayerMove()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        transform.Translate((new Vector3(x, 0, y) * moveSpeed) * Time.deltaTime);
+    }
+    private void PlayerJump()
+    {
+        if (!isJumping)
+        {
+            isJumping = true;
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+        else
+        {
+            return;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+}
+
